82fc78362e64b7a9fa510f0d719ed045c7fb7b06
Kristian Skov Johansen <kris701kj@gmail.com>
2024-05-09 21:07:54 +0200

    Add project files.

---
+++ PlanVal/PlanValidator.cs
@@ -0,0 +1,40 @@
+﻿using PDDLSharp.Models.FastDownward.Plans;
+using PDDLSharp.Models.PDDL;
+
+namespace PlanVal
+{
+    public class PlanValidator
+    {
+        public int Step { get; internal set; }
+        public string ValidationError { get; internal set; } = "";
+
+        public PlanValidator()
+        {
+        }
+
+        public bool Validate(ActionPlan plan, PDDLDecl decl)
+        {
+            Step = 0;
+            ValidationError = "";
+            var simulator = new StateSpaceSimulator(decl);
+            try
+            {
+                foreach (var step in plan.Plan)
+                {
+                    var argStr = new List<string>();
+                    foreach (var arg in step.Arguments)
+                        argStr.Add(arg.Name);
+
+                    simulator.Step(step.ActionName, argStr.ToArray());
+                    Step++;
+                }
+                return simulator.State.IsInGoal();
+            }
+            catch (Exception ex)
+            {
+                ValidationError = ex.Message;
+                return false;
+            }
+        }
+    }
+}
